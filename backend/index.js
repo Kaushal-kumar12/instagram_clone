72214c03d41415c0e9c18d7c0501f038eedeb4da
Kaushal Kumar <kaushalkumar78256@gmail.com>
2025-07-12 09:08:24 +0530

    Initial commit: Full Stack MERN App with frontend and backend

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+import postRoute from "./routes/post.route.js";
+import messageRoute from "./routes/message.route.js";
+import { app, server } from "./socket/socket.js";
+import path from "path";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const _dirname = path.resolve();
+
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(urlencoded({ extended: true }));
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+// API routes
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/post", postRoute);
+app.use("/api/v1/message", messageRoute);
+
+// Serve frontend build
+app.use(express.static(path.join(_dirname, "frontend", "dist")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+// Start server
+server.listen(PORT, () => {
+  connectDB();
+  console.log(`server is listen at port ${PORT}`);
+});
